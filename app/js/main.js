3375834795944bc7fff4e52ad401500ac149d1fe
Yuriy Yeliseyev <yuriye@mail.ru>
2021-03-23 22:54:06 +1200

    top-slider added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function ( ) {
-  
+$(function () {
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false
+    // fade: true,
+    // autoplay: true,
+    // autoplaySpeed: 2000
+  });
 })
